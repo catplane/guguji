76ab2da54ac2f6f6ac5b1d194cd24c28f1be3670
catplane <nihaoyawoshixixida@gmail.com>
2019-01-03 17:32:55 +0800

    首页右上角内容逻辑显示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,13 +1,20 @@
+from info.models import User
 from info.modules.index import index_bp
-from flask import current_app, render_template
+from flask import current_app, render_template, session
 from info import redis_store
 
 
 @index_bp.route('/')
 def index():
-    # 返回渲染模板文件
-    print(current_app.url_map)
-    return render_template("news/index.html")
+    user_id = session.get("user_id")
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.erro(e)
+
+    return render_template("news/index.html", data={"user_info": user.to_dict() if user else None})
 
 
 @index_bp.route('/favicon.ico')
