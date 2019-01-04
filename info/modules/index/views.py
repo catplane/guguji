25a5c6aedda74e8ec609ce1d90b48abd7b17ed0c
catplane <nihaoyawoshixixida@gmail.com>
2019-01-04 11:43:12 +0800

    新闻分类

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,8 @@
-from info.models import User, News
+from info.models import User, News, Category
 from info.modules.index import index_bp
-from flask import current_app, render_template, session
+from flask import current_app, render_template, session, jsonify
 from info import redis_store, constants
+from info.response_code import RET
 
 
 @index_bp.route('/')
@@ -21,9 +22,20 @@ def index():
     click_news_list = []
     for news in news_list if news_list else []:
         click_news_list.append(news.to_basic_dict())
+    try:
+        categories = Category.query.all()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询分类数据异常")
+    # 分类对象列表转换成字典列表
+    category_dict_list = []
+    for category in categories if categories else []:
+        category_dict_list.append(category.to_dict())
+
     data = {
         "user_info":user.to_dict() if user else None,
         "click_news_list": click_news_list,
+        "categories": category_dict_list
     }
 
     return render_template("news/index.html", data=data)
